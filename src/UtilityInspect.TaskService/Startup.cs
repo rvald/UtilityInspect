d1fc446aa3cf3f988316ff79362f0082d18bb944
Rene Valdes <rvald@Renes-MBP.attlocal.net>
2021-02-08 03:35:30 -0500

    Refactored Project

--- src/UtilityInspect.TaskService/Startup.cs
+++ src/UtilityInspect.TaskService/Startup.cs
@@ -44,11 +44,7 @@ namespace UtilityInspect.TaskService
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UtilityInspect.TaskService v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
